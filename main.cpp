66104aadbbac312fced296175bb2d3069dd618c5
dmitrijegorov <46870363+dmitrijegorov@users.noreply.github.com>
2019-06-09 19:59:32 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 
 int main() {
 
-    schwertkampfer schwertkampfer1(74, 1, 5), schwertkampfer2(74, 1, 15);;
-    magier magier1(51, 2, 4);
-    bogenschutze bogenschutze1(55, 2, 3);
+    schwertkampfer schwertkampfer1(74, 1, 5), schwertkampfer2(74, 1, 15), schwertkampfer3(60, 2, 14);
+    bogenschutze bogenschutze1(55, 2, 3), bogenschutze2(60, 4, 10);
+    magier magier1(51, 5, 4), magier2(80, 3, 3);
     auto * c1= (schwertkampfer *)malloc(1 * sizeof(schwertkampfer));
     if (c1== nullptr)
         return -1;
@@ -19,6 +19,11 @@ int main() {
         return -1;
     c3[0]=magier1;
     schwertkampfer1.action(c1, c2, c3, 1, 1, 1);
+    std :: cout << schwertkampfer1.x << "\n"<< schwertkampfer1.health <<"\n"<< schwertkampfer1.level << "\n";
+    c2[0]=bogenschutze2;
+    bogenschutze1.action(c1, c2, c3, 1, 1, 1);
+    c3[0]=magier2;
+    magier1.action(c1, c2, c3, 1, 1, 1);
 
 
     return 0;
