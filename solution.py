1a6b6765b6d76596c24533215ffa2fcb4dd8e32a
dmitrijegorov <46870363+dmitrijegorov@users.noreply.github.com>
2019-11-17 17:35:36 +0300

    Update 6. Проходной балл

--- solution.py
+++ solution.py
@@ -1,16 +1,16 @@
 file_in = open("input.txt", mode="r", encoding="utf-8")
 k = (file_in.readline())
 k = int(k)
-arr = file_in.readlines()
-arr_new = []
-arr = [i.split()[-3:] for i in arr]
-arr = [list(map(int, i)) for i in arr]
+m = file_in.readlines()
+new_m = []
+m = [i.split()[-3:] for i in m]
+m = [list(map(int, i)) for i in m]
 total = []
-for i in range(len(arr)):
-    if (min(arr[i]) >= 40):
-        arr_new.append(arr[i])
-for i in range(len(arr_new)):
-    total.append(sum(arr_new[i]))
+for i in range(len(m)):
+    if (min(m[i]) >= 40):
+        new_m.append(m[i])
+for i in range(len(new_m)):
+    total.append(sum(new_m[i]))
 total = sorted(total)
 file_out = open("output.txt", mode="w", encoding="utf-8")
 if k == 0:
@@ -19,13 +19,14 @@ elif len(total) <= k:
     file_out.write("0")
 else:
     flag = 0
-    if total[-k] == total[-k - 1]:
+    if total[-k] != total[-k - 1]:
+        file_out.write(str(total[-k]))
+    else:
         for i in range(-k - 1, 0):
             if total[i] != total[-k]:
                 file_out.write(str(total[i]))
                 flag = 1
+                break
         if flag != 1:
             file_out.write("1")
-    else:
-        file_out.write(str(total[-k]))
 file_out.close()
